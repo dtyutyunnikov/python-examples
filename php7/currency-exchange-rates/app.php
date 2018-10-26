67c9a5a7d6dcbdeea941fb36a9779f7a4f78d724
bugness <bugness@yandex.ru>
2018-10-27 00:04:46 +0700

    currency-exchange-rates: added cache

--- php7/currency-exchange-rates/app.php
+++ php7/currency-exchange-rates/app.php
@@ -1,9 +1,10 @@
 <?php
 
 (new class {
-    const TIME_INTERVAL = 10; // Minutes
+    const TIME_INTERVAL = '10 minutes';
     const FILE_LOCATION = 'http://www.cbr.ru/scripts/XML_daily.asp';
 //    const FILE_LOCATION = __DIR__ . '/XML_daily.xml';
+    const CACHE_FILE    = __DIR__ . '/data.json';
 
     protected $charCodes = ['USD', 'EUR'];
 
@@ -13,8 +14,24 @@
         $this->output($data);
     }
 
-    public function loadData()
+    private function loadData()
     {
+        $cache = (file_exists(self::CACHE_FILE) ? json_decode(file_get_contents(self::CACHE_FILE), true) : [
+            'lock'        => 0,
+            'last_update' => '0000-00-00T00:00:00+00:00',
+            'data'        => [],
+        ]);
+
+        $now    = new DateTime;
+        $update = DateTime::createFromFormat(DATE_W3C, $cache['last_update'])->modify('+' . self::TIME_INTERVAL);
+        return ($cache['lock'] || $update > $now ? $cache['data'] : $this->updateData($cache));
+    }
+
+    private function updateData(array $cache)
+    {
+        $cache['lock'] = 1;
+        file_put_contents(self::CACHE_FILE, json_encode($cache));
+
         $raw = file_get_contents(self::FILE_LOCATION, false, stream_context_create(['http' => ['header' => 'Accept: application/xml']]));
         $xml = simplexml_load_string($raw);
 
@@ -26,10 +43,15 @@
             $result[(string) $row->CharCode] = (string) $row->Value;
         }
 
+        $cache['lock']        = 0;
+        $cache['last_update'] = (new DateTime)->format(DATE_W3C);
+        $cache['data']        = $result;
+        file_put_contents(self::CACHE_FILE, json_encode($cache));
+
         return $result;
     }
 
-    public function output($data)
+    private function output($data)
     {
         echo self::FILE_LOCATION . PHP_EOL;
         foreach ($data as $name => $value) {
